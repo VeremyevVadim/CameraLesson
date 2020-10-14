1454530085e8d34228a56c6a9f47c2ec15aca7d7
VeremyevVadim <qwertyvadimv@mail.ru>
2020-10-14 15:24:12 +0300

    Character left/right rotation

--- CameraLesson/Assets/Scripts/CharacterControl.cs
+++ CameraLesson/Assets/Scripts/CharacterControl.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class CharacterControl : MonoBehaviour
 {
-    [SerializeField]
-    private float movementSpeed = 5f;
+    [SerializeField] private float movementSpeed = 5f;
+    [SerializeField] private float mouseSensibility = 45f;
     
     private Transform _characterTransform;
     private void Start()
@@ -23,8 +23,23 @@ public class CharacterControl : MonoBehaviour
         }
     }
     
+    private Vector3 _mouseMovementVector
+    {
+        get
+        {
+            var horizontal = Input.GetAxis("Mouse X");
+            var vertical = Input.GetAxis("Mouse Y");
+            return new Vector3(horizontal, 0.0f, 0.0f);
+        }
+    }
     private void FixedUpdate()
     {
         _characterTransform.Translate(_movementVector * (movementSpeed * Time.fixedDeltaTime));
+        _characterTransform.rotation *= Quaternion.Euler(
+            0,
+            _mouseMovementVector.x * mouseSensibility * Time.fixedDeltaTime,
+            0
+            );
+        
     }
 }
