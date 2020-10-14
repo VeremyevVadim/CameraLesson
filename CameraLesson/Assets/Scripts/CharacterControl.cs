d41f56d5a12eca67e3e8634cfe0527f1ae97f8dc
VeremyevVadim <qwertyvadimv@mail.ru>
2020-10-14 16:41:02 +0300

    Camera zoom added

--- CameraLesson/Assets/Scripts/CharacterControl.cs
+++ CameraLesson/Assets/Scripts/CharacterControl.cs
@@ -2,25 +2,33 @@
 using System.Collections.Generic;
 using System.Data;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class CharacterControl : MonoBehaviour
 {
     [SerializeField] private float movementSpeed = 5f;
     [SerializeField] private float mouseSensibility = 45f;
     [SerializeField] private float cameraLimitY = 90f;
-
+    [SerializeField] private float zoomDamping = 5.0f;
     private float _x = 0f;
     private float _y = 90f;
+
     private Transform _characterTransform;
     
     private Camera[] _cameras;
-    private Transform _gunCamera;
-    
+    private float _baseCameraFOV = 60f;
+    private float _zoomCameraFOV = 30f;
+    private bool _isZoom = false;
+
     private void Start()
     {
         _characterTransform = transform;
         cameraLimitY = Mathf.Abs(cameraLimitY);
-        _cameras = GameObject.FindObjectsOfType<Camera>();
+        _cameras = FindObjectsOfType<Camera>();
+
+        if (_cameras.Length == 0) return;
+        _baseCameraFOV = _cameras[0].fieldOfView;
+        _zoomCameraFOV = _baseCameraFOV / 2;
     }
 
     private Vector3 _movementVector
@@ -58,9 +66,29 @@ public class CharacterControl : MonoBehaviour
         _characterTransform.localEulerAngles = new Vector3(-_y, _x, 0.0f);
     }
 
+    private void CameraSmoothZoom()
+    {
+        var wantedZoom = _isZoom ? _zoomCameraFOV : _baseCameraFOV;
+        
+        foreach (var currentCamera in _cameras)
+        {
+            var currentFOV = currentCamera.fieldOfView;
+            currentFOV = Mathf.Lerp(currentFOV, wantedZoom, zoomDamping * Time.deltaTime);
+            currentCamera.fieldOfView = currentFOV;
+        }
+    }
+
     private void FixedUpdate()
     {
         CharacterMove();
         CameraRotate();
+        if (Input.GetButton("Fire2"))
+        {
+            _isZoom = true;
+        }
+
+        CameraSmoothZoom();
+        _isZoom = false;
     }
+    
 }
