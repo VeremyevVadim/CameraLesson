c34791861b6e16fe77e83d634d12f4c0fd86f268
VeremyevVadim <qwertyvadimv@mail.ru>
2020-10-14 15:14:56 +0300

    Character movement

---
+++ CameraLesson/Assets/Scripts/CharacterControl.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterControl : MonoBehaviour
+{
+    [SerializeField]
+    private float movementSpeed = 5f;
+    
+    private Transform _characterTransform;
+    private void Start()
+    {
+        _characterTransform = transform;
+    }
+
+    private Vector3 _movementVector
+    {
+        get
+        {
+            var horizontal = Input.GetAxis("Horizontal");
+            var vertical = Input.GetAxis("Vertical");
+            return new Vector3(horizontal, 0.0f, vertical);
+        }
+    }
+    
+    private void FixedUpdate()
+    {
+        _characterTransform.Translate(_movementVector * (movementSpeed * Time.fixedDeltaTime));
+    }
+}
