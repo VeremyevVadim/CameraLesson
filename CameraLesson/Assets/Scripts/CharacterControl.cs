292b22738ac24de4665f5545c481972f65e941b6
VeremyevVadim <qwertyvadimv@mail.ru>
2020-10-14 16:06:24 +0300

    Up/Down camera control added

--- CameraLesson/Assets/Scripts/CharacterControl.cs
+++ CameraLesson/Assets/Scripts/CharacterControl.cs
@@ -1,16 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Data;
 using UnityEngine;
 
 public class CharacterControl : MonoBehaviour
 {
     [SerializeField] private float movementSpeed = 5f;
     [SerializeField] private float mouseSensibility = 45f;
-    
+    [SerializeField] private float cameraLimitY = 90f;
+
+    private float _x = 0f;
+    private float _y = 0f;
     private Transform _characterTransform;
+    
+    private Camera[] _cameras;
+    private Transform _gunCamera;
+    
     private void Start()
     {
         _characterTransform = transform;
+        cameraLimitY = Mathf.Abs(cameraLimitY);
+        _cameras = GameObject.FindObjectsOfType<Camera>();
     }
 
     private Vector3 _movementVector
@@ -29,17 +39,18 @@ public class CharacterControl : MonoBehaviour
         {
             var horizontal = Input.GetAxis("Mouse X");
             var vertical = Input.GetAxis("Mouse Y");
-            return new Vector3(horizontal, 0.0f, 0.0f);
+            return new Vector3(horizontal, vertical, 0.0f);
         }
     }
     private void FixedUpdate()
     {
         _characterTransform.Translate(_movementVector * (movementSpeed * Time.fixedDeltaTime));
-        _characterTransform.rotation *= Quaternion.Euler(
-            0,
-            _mouseMovementVector.x * mouseSensibility * Time.fixedDeltaTime,
-            0
-            );
         
+        var rotationSpeed = mouseSensibility * Time.fixedDeltaTime;
+        var mouseMovementVector = _mouseMovementVector;
+        _x = _characterTransform.localEulerAngles.y + mouseMovementVector.x * rotationSpeed;
+        _y += mouseMovementVector.y * rotationSpeed;
+        _y = Mathf.Clamp(_y, -cameraLimitY, cameraLimitY);
+        _characterTransform.localEulerAngles = new Vector3(-_y, _x, 0.0f);
     }
 }
